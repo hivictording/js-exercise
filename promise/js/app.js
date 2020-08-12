88d5d151b9d0e1d85a4cc3d8eff31326099fafb8
hivictording <jianboding@outlook.com>
2020-08-12 18:15:18 -0400

    20200812 2nd commit

--- promise/js/app.js
+++ promise/js/app.js
@@ -1,20 +1,33 @@
 const container = document.querySelector(".container");
 const h1One = document.querySelector(".one");
-const h1Two = document.querySelector(".twoo");
+const h1Two = document.querySelector(".two");
 const h1Three = document.querySelector(".three");
 const buttonChange = document.querySelector(".change");
 
-buttonChange.addEventListener("click", () =>
-  changeColor(h1One, 2000, "green")
-    .then(() => changeColor(h1Two, 2000, "red"))
-    .then(() => changeColor(h1Three, 2000, "blue"))
-    .catch((error) => {
-      let h1Error = document.createElement("H1");
-      h1Error.innerText = error;
-      h1Error.style.color = "red";
-      container.appendChild(h1Error);
-    })
-);
+// buttonChange.addEventListener("click", () =>
+//   changeColor(h1One, 2000, "green")
+//     .then(() => changeColor(h1Two, 2000, "red"))
+//     .then(() => changeColor(h1Three, 2000, "blue"))
+//     .catch((error) => {
+//       let h1Error = document.createElement("H1");
+//       h1Error.innerText = error;
+//       h1Error.style.color = "red";
+//       container.appendChild(h1Error);
+//     })
+// );
+
+buttonChange.addEventListener("click", async () => {
+  try {
+    await changeColor(h1One, 1000, "red");
+    await changeColor(h1Two, 1000, "blue");
+    await changeColor(h1Three, 1000, "green");
+  } catch (error) {
+    let h1Error = document.createElement("H1");
+    h1Error.innerText = error;
+    h1Error.style.color = "red";
+    container.appendChild(h1Error);
+  }
+});
 
 function changeColor(element, time, color) {
   return new Promise((resolve, reject) => {
